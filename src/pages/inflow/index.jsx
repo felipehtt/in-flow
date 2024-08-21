e95bdeec5dbf0166521886b1f5f0422db9437965
davi <daviforever17@gmail.com>
2024-08-21 15:28:29 -0300

    in

---
+++ src/pages/inflow/index.jsx
@@ -0,0 +1,40 @@
+import './index.scss';
+
+function InFlow() {
+
+  return (
+
+    <div className="pagina-inFlow">
+
+      <div className='esq'>
+
+          <h1>In-Flow</h1>
+          <h3>Frontend Developers</h3>
+
+          <ul id='menu'>
+            <li><a href="#sobre">Sobre</a></li>
+            <li><a href="#experiencias">Experiências</a></li>
+            <li>Projetos</li>
+          </ul>
+
+          <p>Somos especialistas em desenvolvimento front-end.</p>
+
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+
+      </div>
+
+      <div className='dir'>
+
+
+
+      </div>
+
+    </div>
+
+  );
+
+}
+
+export default InFlow;
