3d95bdc9c85b2df70267944b41f1f4e71cc3595c
davi <daviforever17@gmail.com>
2024-08-27 12:02:24 -0300

    in3

--- src/pages/inflow/index.jsx
+++ src/pages/inflow/index.jsx
@@ -25,7 +25,35 @@ function InFlow() {
 
       <div className='dir'>
 
-        
+        <section id='sobre'>
+          
+          <h3>Sobre</h3>
+
+          <p className='prgrf'>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+          </p>
+
+        </section>
+
+        <section id='experiencias'>
+
+          <h3>Experiências</h3>
+
+          <p className='prgrf'>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+          </p>
+
+        </section>
+
+        <section id='projetos'>
+
+          <h3>Projetos</h3>
+
+          <p className='prgrf'>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+          </p>
+
+        </section>
 
       </div>
 
